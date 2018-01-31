cc9c3aadfecebe2d74b495b744e68b4d23f5e303
mob-omori <omori@mobcast.jp>
2018-01-31 05:41:49 +0000

    only gc

--- jstat_exporter.go
+++ jstat_exporter.go
@@ -35,11 +35,8 @@ var latestJstat = make(map[string]string)
 
 type Exporter struct {
 	newMax     prometheus.Gauge
-	newCommit  prometheus.Gauge
 	oldMax     prometheus.Gauge
-	oldCommit  prometheus.Gauge
 	metaMax    prometheus.Gauge
-	metaCommit prometheus.Gauge
 	metaUsed   prometheus.Gauge
 	oldUsed    prometheus.Gauge
 	sv0Used    prometheus.Gauge
@@ -56,30 +53,15 @@ func NewExporter() *Exporter {
 			Name:      "newMax",
 			Help:      "newMax",
 		}),
-		newCommit: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "newCommit",
-			Help:      "newCommit",
-		}),
 		oldMax: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "oldMax",
 			Help:      "oldMax",
 		}),
-		oldCommit: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "oldCommit",
-			Help:      "oldCommit",
-		}),
 		metaMax: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "metaMax",
 			Help:      "metaMax",
-		}),
-		metaCommit: prometheus.NewGauge(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "metaCommit",
-			Help:      "metaCommit",
 		}),
 		metaUsed: prometheus.NewGauge(prometheus.GaugeOpts{
 			Namespace: namespace,
@@ -122,11 +104,8 @@ func NewExporter() *Exporter {
 // Describe implements the prometheus.Collector interface.
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	e.newMax.Describe(ch)
-	e.newCommit.Describe(ch)
 	e.oldMax.Describe(ch)
-	e.oldCommit.Describe(ch)
 	e.metaMax.Describe(ch)
-	e.metaCommit.Describe(ch)
 	e.metaUsed.Describe(ch)
 	e.oldUsed.Describe(ch)
 	e.sv0Used.Describe(ch)
@@ -138,109 +117,7 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 
 // Collect implements the prometheus.Collector interface.
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
-	e.JstatGccapacity(ch)
-	e.JstatGcold(ch)
-	e.JstatGcnew(ch)
 	e.JstatGc(ch)
-}
-
-func (e *Exporter) JstatGccapacity(ch chan<- prometheus.Metric) {
-	mu.RLock()
-	defer mu.RUnlock()
-
-	line, ok := latestJstat["-gccapacity"]
-
-	if ok && line != "" {
-		parts := strings.Fields(line)
-		newMax, err := strconv.ParseFloat(parts[1], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.newMax.Set(newMax)
-		e.newMax.Collect(ch)
-		newCommit, err := strconv.ParseFloat(parts[2], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.newCommit.Set(newCommit)
-		e.newCommit.Collect(ch)
-		oldMax, err := strconv.ParseFloat(parts[7], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.oldMax.Set(oldMax)
-		e.oldMax.Collect(ch)
-		oldCommit, err := strconv.ParseFloat(parts[8], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.oldCommit.Set(oldCommit)
-		e.oldCommit.Collect(ch)
-		metaMax, err := strconv.ParseFloat(parts[11], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.metaMax.Set(metaMax)
-		e.metaMax.Collect(ch)
-		metaCommit, err := strconv.ParseFloat(parts[12], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.metaCommit.Set(metaCommit)
-		e.metaCommit.Collect(ch)
-	}
-}
-
-func (e *Exporter) JstatGcold(ch chan<- prometheus.Metric) {
-	mu.RLock()
-	defer mu.RUnlock()
-
-	line, ok := latestJstat["-gcold"]
-
-	if ok && line != "" {
-		parts := strings.Fields(line)
-		metaUsed, err := strconv.ParseFloat(parts[1], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.metaUsed.Set(metaUsed) // MU: Metaspace utilization (kB).
-		e.metaUsed.Collect(ch)
-		oldUsed, err := strconv.ParseFloat(parts[5], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.oldUsed.Set(oldUsed) // OU: Old space utilization (kB).
-		e.oldUsed.Collect(ch)
-	}
-}
-
-func (e *Exporter) JstatGcnew(ch chan<- prometheus.Metric) {
-	mu.RLock()
-	defer mu.RUnlock()
-
-	line, ok := latestJstat["-gcnew"]
-
-	if ok && line != "" {
-		parts := strings.Fields(line)
-		sv0Used, err := strconv.ParseFloat(parts[2], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.sv0Used.Set(sv0Used)
-		e.sv0Used.Collect(ch)
-		sv1Used, err := strconv.ParseFloat(parts[3], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.sv1Used.Set(sv1Used)
-		e.sv1Used.Collect(ch)
-		edenUsed, err := strconv.ParseFloat(parts[8], 64)
-		if err != nil {
-			log.Fatal(err)
-		}
-		e.edenUsed.Set(edenUsed)
-		e.edenUsed.Collect(ch)
-	}
 }
 
 func (e *Exporter) JstatGc(ch chan<- prometheus.Metric) {
@@ -251,6 +128,62 @@ func (e *Exporter) JstatGc(ch chan<- prometheus.Metric) {
 
 	if ok && line != "" {
 		parts := strings.Fields(line)
+		newMax, err := strconv.ParseFloat(parts[4], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.newMax.Set(newMax)
+		e.newMax.Collect(ch)
+
+		oldMax, err := strconv.ParseFloat(parts[6], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.oldMax.Set(oldMax)
+		e.oldMax.Collect(ch)
+
+		metaMax, err := strconv.ParseFloat(parts[8], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.metaMax.Set(metaMax)
+		e.metaMax.Collect(ch)
+
+		metaUsed, err := strconv.ParseFloat(parts[9], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.metaUsed.Set(metaUsed)
+		e.metaUsed.Collect(ch)
+
+		oldUsed, err := strconv.ParseFloat(parts[7], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.oldUsed.Set(oldUsed)
+		e.oldUsed.Collect(ch)
+
+		sv0Used, err := strconv.ParseFloat(parts[1], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.sv0Used.Set(sv0Used)
+		e.sv0Used.Collect(ch)
+
+		sv1Used, err := strconv.ParseFloat(parts[3], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.sv1Used.Set(sv1Used)
+		e.sv1Used.Collect(ch)
+
+		edenUsed, err := strconv.ParseFloat(parts[5], 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+		e.edenUsed.Set(edenUsed)
+		e.edenUsed.Collect(ch)
+
 		fgcTimes, err := strconv.ParseFloat(parts[14], 64)
 		if err != nil {
 			log.Fatal(err)
@@ -264,18 +197,6 @@ func (e *Exporter) JstatGc(ch chan<- prometheus.Metric) {
 		e.fgcSec.Set(fgcSec)
 		e.fgcSec.Collect(ch)
 	}
-}
-
-func RunJstatGccapacity(jstatPath, target, interval string) {
-	runCommand(jstatPath, "-gccapacity", target, interval)
-}
-
-func RunJstatGcold(jstatPath, target, interval string) {
-	runCommand(jstatPath, "-gcold", target, interval)
-}
-
-func RunJstatGcnew(jstatPath, target, interval string) {
-	runCommand(jstatPath, "-gcnew", target, interval)
 }
 
 func RunJstatGc(jstatPath, target, interval string) {
@@ -378,9 +299,6 @@ func main() {
 	flag.Parse()
 
 	go RunJstatGc(*jstatPath, *target, *interval)
-	go RunJstatGcnew(*jstatPath, *target, *interval)
-	go RunJstatGcold(*jstatPath, *target, *interval)
-	go RunJstatGccapacity(*jstatPath, *target, *interval)
 
 	exporter := NewExporter()
 	prometheus.MustRegister(exporter)
